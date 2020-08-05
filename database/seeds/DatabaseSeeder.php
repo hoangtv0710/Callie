95191df4cd6bb26ae8e2b0eb78b474ac320486dc
hoangtv0710 <tvhkaizen@gmail.com>
2020-08-05 19:59:51 +0700

    make_seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(SubCategorySeeder::class);
+        $this->call(PostSeeder::class);
     }
 }
