6768dffccce6a8fae56aaac632a048bfa07b5b2c
hoangtv0710 <tvhkaizen@gmail.com>
2020-08-12 16:10:06 +0700

    check_login_admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Http\Requests\LoginAdminRequest;
+use Session;
 
 class AdminController extends Controller
 {
     public function index()
     {
     	return view('admin.index');
+    }
+
+    public function login()
+    {
+        return view('admin.login');
+    }
+
+    public function postLogin(LoginAdminRequest $request)
+    {
+        $data = $request->only(['email', 'password']);
+
+        $check_login = Auth::attempt($data);
+
+        if ($check_login == false) 
+        {
+            return redirect()->back()->with('error', 'Sai tài khoản hoặc mật khẩu !');
+        } 
+        elseif (Auth::user()->role == 0) 
+        {
+            return redirect()->back()->with('error', 'Bạn không đủ quyền vào trang này !');
+        } 
+        elseif (Auth::user()->status != 1) 
+        {
+            return redirect()->back()->with('error', 'Tài khoản của bạn đã bị khoá !');
+        } 
+        else {
+            return redirect()->route('admin');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        Session::flush();
+
+        return redirect()->route('admin.login');
     }
 }
---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Đăng nhập</title>
+    <base href="{{ asset('') }}">
+
+    <link href="assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link href="assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/toastr.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <a href="{{ url('/') }}">
+                                            <h1 class="h4 mb-4"><img src="images/logo.png" alt=""></h1>
+                                        </a>
+                                    </div>
+                                    <form class="user" method="post"
+                                        action="{{ route('admin.post_login') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <input type="email" name="email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Enter Email Address..." value="{{ old('email') }}">
+                                            @error('email')
+                                                <span class="ml-3 text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Password">
+                                            @error('password')
+                                                <span class="ml-3 text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+    @include('admin.layouts.bottom_asset')
+</body>
